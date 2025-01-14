2c6a5f5cb5c0db928633ece4377c65928d0d2d4c
Maxx1600 <149877659+Maxx1600@users.noreply.github.com>
2025-01-14 19:10:08 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,2 @@
+<div formsappId="678696155b87765987eb6b82"></div>
+<script src="https://forms.app/static/embed.js" type="text/javascript" async defer onload="new formsapp('678696155b87765987eb6b82', 'fullscreen', {}, 'https://2c4tbyb6.forms.app');"></script>
